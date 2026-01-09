b6da5e78a477a7057a692f9ea0bb7363e0b628f0
Volodymyr Kozik <volodymyr.kozik@cantire.com>
2026-01-09 23:26:21 +0200

    Test commit 4

--- calcbuilder-house/src/main/java/ua/in/vkozik/calcbuilder/house/service/impl/HouseCalcImpl.java
+++ calcbuilder-house/src/main/java/ua/in/vkozik/calcbuilder/house/service/impl/HouseCalcImpl.java
@@ -1,42 +1,55 @@
 package ua.in.vkozik.calcbuilder.house.service.impl;
+
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import ua.in.vkozik.calcbuilder.house.clients.BasementClient;
 import ua.in.vkozik.calcbuilder.house.clients.RoofClient;
 import ua.in.vkozik.calcbuilder.house.clients.WallClient;
 import ua.in.vkozik.calcbuilder.house.data.BudgetHouse;
 import ua.in.vkozik.calcbuilder.house.service.HouseCalc;
+
 @Slf4j
 @Service
-public class  HouseCalcImpl implements HouseCalc {
+public class   HouseCalcImpl implements HouseCalc {
     @Autowired
     BasementClient basementClient;
+
     @Autowired
     RoofClient roofClient;
+
     @Autowired
     WallClient wallClient;
+
     @Override
     @HystrixCommand(fallbackMethod = "calculateFallback")
     public BudgetHouse calculate(double length, double width, int numberOfLevels, double roomHeight) {
         Double basementCost = calculateBasement(length * width, numberOfLevels);
         Double wallsCost = calculateWalls(length, width, numberOfLevels, roomHeight);
         Double roofCost = calculateRoof(length, width);
+
         return new BudgetHouse(basementCost, wallsCost, roofCost);
     }
-    public BudgetHouse calculateFallback(double length, double width, int numberOfLevels, double roomHeight, Throwable exception) {
+
+    public BudgetHouse calculateFallback(double length, double width, int numberOfLevels, double roomHeight,
+                                         Throwable exception) {
         return new BudgetHouse("There are problems with some services. Please try later. "
                 + exception.getMessage());
     }
+
     public BudgetHouse calculateForUser(String userId, String password) {
         log.info("calculate for user {} with password: {}", userId, password);
     }
+
     private Double calculateBasement(double square, int numberOfLevels) {
         double height = 0.6;
         double depth = 0.5 / numberOfLevels;
+
         return basementClient.calculate(square, height, depth);
     }
+
     private Double calculateWalls(double length, double width, int numberOfLevels, double roomHeight) {
         Double wallCost = 0.0;
         for (int i = 0; i < numberOfLevels; i++) {
@@ -52,9 +65,12 @@ public class  HouseCalcImpl implements HouseCalc {
             }
             roomHeight += 0.3;
         }
+
         return wallClient.calculate(length, width, numberOfLevels, roomHeight);
     }
+
     private Double calculateRoof(double length, double width) {
+
         return roofClient.calculate(length, width);
     }
 }
